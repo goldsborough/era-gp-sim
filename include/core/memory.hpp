93a96cc3434703c2d702b672d96e5fd88caa4459
ProfPille <danield.riedel@tum.de>
2016-06-07 22:21:24 +0200

    better header

---
+++ include/core/memory.hpp
@@ -0,0 +1,27 @@
+#ifndef ERAGPSIM_CORE_MEMORY_H
+#define ERAGPSIM_CORE_MEMORY_H
+
+#include <vector>
+
+//#include "memoryvalue.h"
+typedef std::vector<bool> MemoryValue;
+
+enum class endian{klittleEndian,kbigEndian};
+
+class Memory
+{
+public:
+    //Creates a new virtual memory object with 'length' memory cells of width 'width' with endianness 'endianness'
+    Memory(int,int,endian);
+    //Creates a new virtual memory object with 'length' memory cells of width 8 with endianness 'endianness'
+    Memory(int,endian);
+    //destructor
+    ~Memory()=default;
+
+    //Return the value of the 'length' memory cells after the memory cell at 'asdress'
+    MemoryValue get(const int, const int=1)const;
+    //Sets the value of the memory cells after the memory cell at 'adress' to 'value'
+    void set(const int,const MemoryValue&);
+};
+
+#endif // ERAGPSIM_CORE_MEMORY_H
