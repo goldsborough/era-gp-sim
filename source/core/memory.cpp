7718a2efb36c8c80e495abfd1f25a558f734c030
ProfPille <danield.riedel@tum.de>
2016-06-08 09:12:38 +0200

    updated formatting

--- source/core/memory.cpp
+++ source/core/memory.cpp
@@ -3,14 +3,13 @@
 #include <iostream>
 
 Memory::Memory(int length,int width,endian endianness){
-    std::cout<<length << "; " << width << "; " << std::endl;
+  std::cout<<length << "; " << width << "; " << std::endl;
 }
 
 Memory::Memory(int length,endian endianness):Memory(length,8,endianness){}
 
 MemoryValue Memory::get(const int adress, const int length)const{
-    return MemoryValue{};
+  return MemoryValue{};
 }
 
-void Memory::set(const int adress,const MemoryValue& value){
-}
+void Memory::set(const int adress,const MemoryValue& value){}
