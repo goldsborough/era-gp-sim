39dcc5dbcf6ae885fd57014e2a9f5c363cedd8f6
ProfPille <danield.riedel@tum.de>
2016-06-08 09:12:15 +0200

    better formatting

--- include/core/memory.hpp
+++ include/core/memory.hpp
@@ -3,25 +3,28 @@
 
 #include <vector>
 
-//#include "memoryvalue.h"
+//#include "memoryvalue.hpp"
 typedef std::vector<bool> MemoryValue;
 
-enum class endian{klittleEndian,kbigEndian};
+enum class endian{kLittleEndian,kBigEndian};
 
-class Memory
-{
+class Memory{
 public:
-    //Creates a new virtual memory object with 'length' memory cells of width 'width' with endianness 'endianness'
-    Memory(int,int,endian);
-    //Creates a new virtual memory object with 'length' memory cells of width 8 with endianness 'endianness'
-    Memory(int,endian);
-    //destructor
-    ~Memory()=default;
+  //Creates a new virtual memory object with 'length' memory cells of width
+  //'width' with endianness 'endianness'
+  Memory(int,int,endian);
+  //Creates a new virtual memory object with 'length' memory cells of width
+  //8 with endianness 'endianness'
+  Memory(int,endian);
+  //destructor
+  ~Memory()=default;
 
-    //Return the value of the 'length' memory cells after the memory cell at 'asdress'
-    MemoryValue get(const int, const int=1)const;
-    //Sets the value of the memory cells after the memory cell at 'adress' to 'value'
-    void set(const int,const MemoryValue&);
+  //Return the value of the 'length' memory cells after the memory cell at
+  //'adress'
+  MemoryValue get(const int, const int=1)const;
+  //Sets the value of the memory cells after the memory cell at 'adress' to
+  //'value'
+  void set(const int,const MemoryValue&);
 };
 
 #endif // ERAGPSIM_CORE_MEMORY_H
