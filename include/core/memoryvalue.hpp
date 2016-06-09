75e4b7c09c40b751d0c1335346c105c7ab73c160
ProfPille <danield.riedel@tum.de>
2016-06-09 22:13:58 +0200

    formatted, added operator== and operator<<

--- include/core/memoryvalue.hpp
+++ include/core/memoryvalue.hpp
@@ -2,14 +2,14 @@
 #define ERAGPSIM_CORE_MEMORYVALUE_H
 
 #include <vector>
+#include <iostream>
 
-class MemoryValue
-{
+class MemoryValue{
 public:
     //Creates a MemoryValue of width 'width' (default=8)
-    MemoryValue(int=8);
+    MemoryValue(int width=8);
     //Creates a MemoryValue with a copy of the std::vector<bool> 'data'
-    MemoryValue(const std::vector<bool>);
+    MemoryValue(const std::vector<bool> data);
     //copy constructor
     MemoryValue(const MemoryValue&)=default;
     //copy operator
@@ -21,20 +21,33 @@ public:
     //destructor
     ~MemoryValue()=default;
 
-    //Returns a reference to the element at specified location 'index', with bounds checking.
-    //If 'index' is not within the range of the container, an exception of type std::out_of_range is thrown.
-    inline std::vector<bool>::reference at(const int);
-    //Returns a reference to the element at specified location 'index', with bounds checking.
-    //If 'index' is not within the range of the container, an exception of type std::out_of_range is thrown.
-    inline std::vector<bool>::const_reference at(const int)const;
-    //Returns a reference to the element at specified location 'index'. No bounds checking is performed.
-    inline std::vector<bool>::reference operator[](const int);
-    //Returns a reference to the element at specified location 'index'. No bounds checking is performed.
-    inline std::vector<bool>::const_reference operator[](const int)const;
+    //Returns a reference to the element at specified location 'index', with
+    //bounds checking.
+    //If 'index' is not within the range of the container, an exception of type
+    //std::out_of_range is thrown.
+    std::vector<bool>::reference at(const int index);
+    //Returns a reference to the element at specified location 'index', with
+    //bounds checking.
+    //If 'index' is not within the range of the container, an exception of type
+    //std::out_of_range is thrown.
+    std::vector<bool>::const_reference at(const int index)const;
+    //Returns a reference to the element at specified location 'index'. No
+    //bounds checking is performed.
+    std::vector<bool>::reference operator[](const int index);
+    //Returns a reference to the element at specified location 'index'. No
+    //bounds checking is performed.
+    std::vector<bool>::const_reference operator[](const int index)const;
     //Toggles each bool in the vector (replaces with its opposite value).
-    inline void flip();
+    void flip();
+
+    //returns true iff second._data==this->_data
+    bool operator==(MemoryValue&);
+    //puts a binary representation of 'this Memory Value'value' into the ostream
+    friend std::ostream& operator<<(std::ostream& stream,
+                                    const MemoryValue& value);
+
 private:
-    std::vector<bool> data;
+    std::vector<bool> _data;
 };
 
 #endif // ERAGPSIM_CORE_MEMORYVALUE_H
