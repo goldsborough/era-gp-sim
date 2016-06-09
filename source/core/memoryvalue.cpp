89aaf362e84efa99a5fe3250f00aeb68715edbe6
ProfPille <danield.riedel@tum.de>
2016-06-09 22:14:17 +0200

    formatted, added operator== and operator<<

--- source/core/memoryvalue.cpp
+++ source/core/memoryvalue.cpp
@@ -1,27 +1,35 @@
 #include "memoryvalue.hpp"
 
-MemoryValue::MemoryValue(int width):data{std::vector<bool>()}{
-    for(int i=width;i-- >0;)data.push_back(false);
-}
+MemoryValue::MemoryValue(int width):_data{std::vector<bool>(width,false)}{}
 
-MemoryValue::MemoryValue(const std::vector<bool> data):data{data}{}
+MemoryValue::MemoryValue(const std::vector<bool> data):_data{data}{}
 
 std::vector<bool>::reference MemoryValue::at(const int index){
-    return data.at(index);
+  return _data.at(index);
 }
 
 std::vector<bool>::const_reference MemoryValue::at(const int index)const{
-    return data.at(index);
+  return _data.at(index);
 }
 
 std::vector<bool>::reference MemoryValue::operator[](const int index){
-    return data[index];
+  return _data[index];
 }
 
-std::vector<bool>::const_reference MemoryValue::operator[](const int index)const{
-    return data[index];
+std::vector<bool>::const_reference MemoryValue::operator[](const int index)const
+{
+  return _data[index];
 }
 
 void MemoryValue::flip(){
-    data.flip();
+  _data.flip();
+}
+
+bool MemoryValue::operator==(MemoryValue& second){
+  return _data==second._data;
+}
+
+std::ostream& operator<<(std::ostream& stream, const MemoryValue& value){
+  for(bool b:value._data)if(b)stream<<'1';else stream<<'0';
+  return stream;
 }
