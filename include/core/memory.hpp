ccd6ed56f29bad05ca01396177b9746100c0698f
ProfPille <danield.riedel@tum.de>
2016-06-09 22:14:48 +0200

    minor formatting

--- include/core/memory.hpp
+++ include/core/memory.hpp
@@ -3,28 +3,29 @@
 
 #include <vector>
 
-//#include "memoryvalue.hpp"
-typedef std::vector<bool> MemoryValue;
+#include "memoryvalue.hpp"
+//typedef std::vector<bool> MemoryValue;//This WILL create bugs
 
 enum class endian{kLittleEndian,kBigEndian};
 
+//the simulated RAM
 class Memory{
 public:
   //Creates a new virtual memory object with 'length' memory cells of width
   //'width' with endianness 'endianness'
-  Memory(int,int,endian);
+  Memory(int length, int width, endian endianness);
   //Creates a new virtual memory object with 'length' memory cells of width
   //8 with endianness 'endianness'
-  Memory(int,endian);
+  Memory(int length, endian endianness);
   //destructor
   ~Memory()=default;
 
   //Return the value of the 'length' memory cells after the memory cell at
   //'adress'
-  MemoryValue get(const int, const int=1)const;
+  MemoryValue get(const int adress, const int length=1)const;
   //Sets the value of the memory cells after the memory cell at 'adress' to
   //'value'
-  void set(const int,const MemoryValue&);
+  void set(const int adress,const MemoryValue& value);
 };
 
 #endif // ERAGPSIM_CORE_MEMORY_H
